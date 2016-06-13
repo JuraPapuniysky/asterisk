37bb966478459e75a18de6cc0c2f04b0ab501904
wsst17 <jurapapunitsky@gmail.com>
2016-06-13 15:22:55 +0300

    ConfbridgeSetSingleVideoSrc add

---
+++ common/amiactions/ConfbridgeSetSingleVideoSrc.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace common\amiactions;
+
+
+use PAMI\Message\Action\ActionMessage;
+
+class ConfbridgeSetSingleVideoSrc extends ActionMessage
+{
+    public function __construct($conference, $channel)
+    {
+        parent::__construct('ConfbridgeSetSingleVideoSrc');
+        $this->setKey('Conference', $conference);
+        $this->setKey('Channel', $channel);
+    }
+}
